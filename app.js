36e9a3907a7b36e1eadb8934dc858abdd8e24467
ahmedmehmood5147 <am9474036@gmail.com>
2024-01-24 05:13:04 +0500

    Assignment
    Fruit image convert to vegetable image

---
+++ app.js
@@ -0,0 +1,6 @@
+// var image_1 = document.getElementById("image1")
+function image_change() {
+    var btn = document.getElementById("button")
+    btn.innerText="VEGETABLES"
+    var image=document.getElementById("image1").src="./vegetable picture.jpg"
+}
